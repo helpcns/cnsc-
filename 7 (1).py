d4cb6d215aa5e8ce7406a6e91702fe8dace318d2
helpcns <114077412+helpcns@users.noreply.github.com>
2022-09-25 20:52:34 +0530

    Update 7 (1).py

--- 7 (1).py
+++ 7 (1).py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
@@ -11,22 +6,13 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 
-# In[ ]:
-
-
 #data =load_iris()
 
-
-# In[2]:
 
 
 iris=load_iris()
 x=iris.data
 y=iris.target
-
-
-# In[3]:
-
 
 #x_train,x_test,y_train,y_test=train_test_split(data.data,data.target,test_size=0.4,random_state=5)
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.4,random_state=5)
@@ -34,26 +20,22 @@ model=KNeighborsClassifier(n_neighbors=5)
 model.fit(x_train,y_train)
 y_pred=model.predict(x_test)
 
-
-# In[4]:
-
-
 print("Acuraccy of KNN\t\t\t\t",accuracy_score(y_pred,y_test))
-
-
-# In[5]:
-
 
 pd.DataFrame({'actual':y_test,'predction':y_pred,'correct classification':(y_test==y_pred)})
 
+count = 0
+for i in range(len(y_pred)):
+    if(y_pred[i]!=y_test[i]):
+        count=count+1
+print(count)
 
-# In[ ]:
 
 
 
 
 
-# In[ ]:
+
 
 
 
